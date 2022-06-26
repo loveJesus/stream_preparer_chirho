aacd465cecb22270ff71dadd3efd5f9944737261
LoveJesus <loveJesus@loveJes.us>
2022-06-25 19:48:13 -0600

    HALLELUJAH - code cleanup, stream settings, aleluya

---
+++ README.md
@@ -0,0 +1,39 @@
+# For God so loved the world, that He gave His only begotten Son, that all who believe in Him should not perish but have everlasting life
+
+## Youtube Video Stream Preparer
+
+Hallelujah, this will set up YouTube's broadcast title and description for a stream that will be at that moment God
+willing.
+
+## How to use
+
+1. Make a virtual environment `python3 -m venv venv_chirho` and activate it `. venv_chirho/bin/activate`
+2. Install all the dependencies `pip install -r requirements_chirho.txt`
+3. Set up [Google oauth2 Web credentials](https://developers.google.com/identity/protocols/oauth2) with access to youtube data api, that can redirect to `http://localhost:4422`.
+4. Copy your `client_secret_*.json` from those credentials to the same directory as this script, name it
+   `client_secret_chirho.json`
+5. Copy sample file `stream_preparer_chirho.sample.yml` to `stream_preparer_chirho.yml` in the same directory as this script and fill it in with your
+   desired video information.
+6. Run `stream_preparer_chirho.py`
+
+#### And some Twitch instructions, aleluya
+
+```shell
+#For God so loved the world, that He gave His only begotten Son, that all who believe in Him should not perish but have everlasting life
+brew install twitchdev/twitch/twitch-cl  # Hallelujah on Macos
+rehash
+twitch configure
+twitch token -u -s "channel:manage:broadcast"
+twitch api get users -q login=loveJesusbrian  # Get broadcaster id...
+twitch api patch channels -q broadcaster_id=257133600 -b '{"title":"aleluya"}'
+```
+
+#### Additional useful info
+
+```shell
+# Hallelujah download google cloud cli https://cloud.google.com/sdk/docs/install
+# Hallelujah youtube api samples https://github.com/youtube/api-samples.git
+pip3 install --upgrade pip
+pip3 install --upgrade google-auth-oauthlib google-auth-httplib2
+pip3 install google-api-python-client
+```
--- youtube_aleluya.py
+++ youtube_aleluya.py
@@ -29,152 +29,179 @@ logger_chirho.addHandler(handler_chirho)
 logger_chirho.info("Starting... ALELUYA")
 
 
+class YouTubeStreamerChirho:
+    """
+    Hallelujah, initialize a youtube stream with title and description
+    """
+
+    def __init__(self):
+        self.youtube_chirho = None
+        self.insert_broadcast_response_chirho: dict = None
+        self.bind_broadcast_response_chirho: dict = None
+
+    def execute_chirho(self):
+        self._google_credential_receiver_chirho()
+        self._initialize_broadcast_chirho()
+
+    def _google_credential_receiver_chirho(self):
+        """
+        Hallelujah - Receive OAUTH 2 credentials from the user.
+        :return:
+        """
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name_chirho = "youtube"
+        api_version_chirho = "v3"
+        client_secrets_file_chirho = "client_secret_chirho.json"
+
+        logger_chirho.info(Fore.BLUE + "Youtube API client created")
+
+        # json_credentials_chirho = """
+        # { ... }
+        # """
+        # credentials_chirho = Credentials.from_authorized_user_info(json.loads(json_credentials_chirho))
+
+        # Get credentials and create an API client
+        flow_chirho = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file_chirho, scopes=scopes_chirho)
+        credentials_chirho = flow_chirho.run_local_server(port=4422)
+
+        # print(credentials_chirho.to_json())
+        self.youtube_chirho = googleapiclient.discovery.build(
+            api_service_name_chirho, api_version_chirho, credentials=credentials_chirho)
+
+    def _initialize_broadcast_chirho(self):
+        """
+        Hallelujah - Initialize a broadcast, set it's children's permissions
+        :return:
+        """
+        self.insert_broadcast_response_chirho = self.youtube_chirho.liveBroadcasts().insert(
+            part="snippet,status,contentDetails",
+            body={
+                "contentDetails": dict(
+                    enableLowLatency=True, ),
+                "snippet": dict(
+                    title="Love Jesus - Servicio Diario 2022-06-25 בְּרֵאשִׁית/Genesis 5 y ΜΑΘΘΑΙΟΝ/Mateo 4",
+                    description="""
+                        Jesucristo es Señor\n\n
+
+                        Aprendamos a leer la Palabra juntamente en las lenguas de los escritos.\n
+
+                        Oración / Lectura de la Palabra /Animo o Exhortación / Santa Cena / Cantar un Himno / Musica de Fondo
+                        """,
+                    scheduledStartTime=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                    scheduledEndTime=(datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime(
+                        '%Y-%m-%dT%H:%M:%SZ'),
+                ),
+                "status": dict(
+                    privacyStatus="private", )  # options.privacy_status
+            }
+        ).execute()
+
+        # Hallelujah set broadcast permissions
+
+        logger_chirho.info(Fore.BLUE + "Broadcast created")
+        logger_chirho.debug(json.dumps(self.insert_broadcast_response_chirho, indent=4, sort_keys=True))
+
+        videos_list_response_chirho = self.youtube_chirho.videos().list(
+            part="snippet,status",
+            id=self.insert_broadcast_response_chirho["id"], ).execute()["items"]
+
+        logger_chirho.debug(json.dumps(videos_list_response_chirho, indent=4, sort_keys=True))
+
+        videos_list_response_chirho[0]["snippet"]["categoryId"] = 27
+        videos_list_response_chirho[0]["status"]["madeForKids"] = False
+        videos_list_response_chirho[0]["status"]["selfDeclaredMadeForKids"] = False
+        self.youtube_chirho.videos().update(
+            part="snippet,status",
+            body=dict(
+                id=self.insert_broadcast_response_chirho["id"],
+                snippet=videos_list_response_chirho[0]["snippet"],
+                status=videos_list_response_chirho[0]["status"])).execute()
+
+        logger_chirho.info("Video Permissions set ALELUYA")
+
+    def _link_stream_response_chirho(self):
+        """
+        Praise the Lord, link broadcast to a desired stream
+        :return:
+        """
+        # insert_stream_response_chirho = youtube_chirho.liveStreams().insert(
+        #     part="snippet,cdn",
+        #     body=dict(
+        #
+        #         snippet=dict(
+        #             title="NEW STREAM 4 TITLE TEST ALELUYA",
+        #             description="NEW STREAM 4 DESCRIPTION TEST ALELUYA",
+        #         ),
+        #         cdn=dict(
+        #             resolution="720p",
+        #             format="720p",
+        #             ingestionType="rtmp",
+        #             frameRate="60fps",
+        #         )
+        #     )
+        # ).execute()
+        #
+        # print(json.dumps(insert_stream_response_chirho, indent=4, sort_keys=True))
+
+        self.bind_broadcast_response_chirho = self.youtube_chirho.liveBroadcasts().bind(
+            part="id,contentDetails",
+            id=self.insert_broadcast_response_chirho["id"],
+            streamId="J880LYhXwEjWGBFMBQTMWg1656203433631884",  # insert_stream_response_chirho["id"],
+        ).execute()
+
+        logger_chirho.info("Broadcast bound to stream ALELUYA")
+
+        logger_chirho.debug(json.dumps(self.bind_broadcast_response_chirho, indent=4, sort_keys=True))
+
+        logger_chirho.info(
+            "Broadcast '%s' was bound to stream '%s' with key  ." % (
+                self.bind_broadcast_response_chirho["id"],
+                self.bind_broadcast_response_chirho["contentDetails"]["boundStreamId"]))
+
+    def _handle_transitions_chirho(self):
+        """
+        Hallelujah - Handle transitions to testing, live, etc.
+        :return:
+        """
+        # broadcast_list_response_chirho = youtube_chirho.liveBroadcasts().list(
+        #     mine=True,
+        #     id=insert_broadcast_response_chirho["id"],
+        #     part="id,snippet,contentDetails,status").execute()
+        # print(
+        #     json.dumps(broadcast_list_response_chirho, indent=4, sort_keys=True))
+        # input("Press Enter to continue... ALELUYA")
+        logger_chirho.info(
+            Fore.GREEN + Back.WHITE + "Make sure stream has started and wait about 10 seconds - Hallelujah" + Fore.RESET)
+        input("Then Press Enter to continue to stream test... ALELUYA")
+
+        broadcast_transition_response_chirho = self.youtube_chirho.liveBroadcasts().transition(
+            part="id,status",
+            broadcastStatus="testing",
+            id=self.insert_broadcast_response_chirho["id"]).execute()
+
+        logger_chirho.debug(
+            json.dumps(broadcast_transition_response_chirho, indent=4, sort_keys=True))
+        logger_chirho.info("Broadcast TESTING BEGUN ALELUYA")
+        logger_chirho.info(
+            Fore.GREEN + Back.WHITE + "Make sure stream has started and wait about 10 seconds - Hallelujah" + Fore.RESET)
+        input("Then Press Enter to continue... ALELUYA")
+
+        broadcast_transition_response_chirho = self.youtube_chirho.liveBroadcasts().transition(
+            part="id,status",
+            broadcastStatus="live",
+            id=self.insert_broadcast_response_chirho["id"]).execute()
+
+        logger_chirho.debug(
+            json.dumps(broadcast_transition_response_chirho, indent=4, sort_keys=True))
+        logger_chirho.info(Fore.BLUE + "Finished - Hallelujah")
+
+
 def main_chirho():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name_chirho = "youtube"
-    api_version_chirho = "v3"
-    client_secrets_file_chirho = "client_secret_28809006910-sf0tknirm06b2l2ibleecoa8rbfarrh9.apps.googleusercontent.com.json"
-
-    # json_credentials_chirho = """
-    # {"token": "ya29.a0ARrdaM-LVWoi5CBomyp31C9jZU4QzI5XXN3A0tcjZPAA6UhJR_md8hEVnrZQmmkZW7AuTgnoLrDFC01q1Uxb5UY7dHjh66zhFyMlC1chmjTbfH6HkrOD4_ZWw6O4bamHVlwJ2TRhE0t2U2dbXbjqwrjMLWvF",
-    # "refresh_token": "1//0dPf7zVAGkBqVCgYIARAAGA0SNwF-L9IrkALBFzeomya7UmeG3VUl0jXBq0h6_pyHLnOnz9Ba81sFx_q-p3EEOvCCUBrCsImh8j8",
-    # "token_uri": "https://oauth2.googleapis.com/token",
-    # "client_id": "28809006910-sf0tknirm06b2l2ibleecoa8rbfarrh9.apps.googleusercontent.com",
-    # "client_secret": "MWVOApcebbHLXn8H80SWP48a", "scopes": ["https://www.googleapis.com/auth/youtube.readonly"],
-    # "expiry": "2022-06-25T23:01:17.305830Z"}
-    # """
-    # credentials_chirho = Credentials.from_authorized_user_info(json.loads(json_credentials_chirho))
-
-    # Get credentials and create an API client
-    flow_chirho = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file_chirho, scopes=scopes_chirho)
-    credentials_chirho = flow_chirho.run_local_server(port=4422)
-
-    # print(credentials_chirho.to_json())
-    youtube_chirho = googleapiclient.discovery.build(
-        api_service_name_chirho, api_version_chirho, credentials=credentials_chirho)
-
-    logger_chirho.info(Fore.BLUE + "Youtube API client created")
-
-    insert_broadcast_response_chirho = youtube_chirho.liveBroadcasts().insert(
-        part="snippet,status,contentDetails",
-        body={
-            "contentDetails": dict(
-                enableLowLatency=True, ),
-            "snippet": dict(
-                title="Love Jesus - Servicio Diario 2022-06-25 בְּרֵאשִׁית/Genesis 5 y ΜΑΘΘΑΙΟΝ/Mateo 4",
-                description="""
-                Jesucristo es Señor\n\n
-
-                Aprendamos a leer la Palabra juntamente en las lenguas de los escritos.\n
-                
-                Oración / Lectura de la Palabra /Animo o Exhortación / Santa Cena / Cantar un Himno / Musica de Fondo
-                """,
-                scheduledStartTime=datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-                scheduledEndTime=(datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime(
-                    '%Y-%m-%dT%H:%M:%SZ'),
-            ),
-            "status": dict(
-                privacyStatus="private", )  # options.privacy_status
-        }
-    ).execute()
-
-    logger_chirho.info(Fore.BLUE + "Broadcast created")
-
-    logger_chirho.debug(json.dumps(insert_broadcast_response_chirho, indent=4, sort_keys=True))
-
-    videos_list_response_chirho = youtube_chirho.videos().list(
-        part="snippet,status",
-        id=insert_broadcast_response_chirho["id"], ).execute()["items"]
-
-    logger_chirho.debug(json.dumps(videos_list_response_chirho, indent=4, sort_keys=True))
-
-    videos_list_response_chirho[0]["snippet"]["categoryId"] = 27
-    videos_list_response_chirho[0]["status"]["madeForKids"] = False
-    videos_list_response_chirho[0]["status"]["selfDeclaredMadeForKids"] = False
-    youtube_chirho.videos().update(
-        part="snippet,status",
-        body=dict(
-            id=insert_broadcast_response_chirho["id"],
-            snippet=videos_list_response_chirho[0]["snippet"],
-            status=videos_list_response_chirho[0]["status"])).execute()
-
-    logger_chirho.info("Video Permissions set ALELUYA")
-
-    # insert_stream_response_chirho = youtube_chirho.liveStreams().insert(
-    #     part="snippet,cdn",
-    #     body=dict(
-    #
-    #         snippet=dict(
-    #             title="NEW STREAM 4 TITLE TEST ALELUYA",
-    #             description="NEW STREAM 4 DESCRIPTION TEST ALELUYA",
-    #         ),
-    #         cdn=dict(
-    #             resolution="720p",
-    #             format="720p",
-    #             ingestionType="rtmp",
-    #             frameRate="60fps",
-    #         )
-    #     )
-    # ).execute()
-    #
-    # print(json.dumps(insert_stream_response_chirho, indent=4, sort_keys=True))
-
-    bind_broadcast_response_chirho = youtube_chirho.liveBroadcasts().bind(
-        part="id,contentDetails",
-        id=insert_broadcast_response_chirho["id"],
-        streamId="J880LYhXwEjWGBFMBQTMWg1656203433631884",  # insert_stream_response_chirho["id"],
-    ).execute()
-
-    logger_chirho.info("Broadcast bound to stream ALELUYA")
-
-    logger_chirho.debug(json.dumps(bind_broadcast_response_chirho, indent=4, sort_keys=True))
-
-    logger_chirho.info(
-        "Broadcast '%s' was bound to stream '%s' with key  ." % (
-            bind_broadcast_response_chirho["id"],
-            bind_broadcast_response_chirho["contentDetails"]["boundStreamId"]))
-
-    # broadcast_list_response_chirho = youtube_chirho.liveBroadcasts().list(
-    #     mine=True,
-    #     id=insert_broadcast_response_chirho["id"],
-    #     part="id,snippet,contentDetails,status").execute()
-    # print(
-    #     json.dumps(broadcast_list_response_chirho, indent=4, sort_keys=True))
-    # input("Press Enter to continue... ALELUYA")
-    logger_chirho.info(Fore.GREEN + "Make sure stream has started and wait about 10 seconds - Hallelujah" + Fore.RESET)
-    input("Then Press Enter to continue to stream test... ALELUYA")
-
-    broadcast_transition_response_chirho = youtube_chirho.liveBroadcasts().transition(
-        part="id,status",
-        broadcastStatus="testing",
-        id=insert_broadcast_response_chirho["id"]).execute()
-
-    logger_chirho.debug(
-        json.dumps(broadcast_transition_response_chirho, indent=4, sort_keys=True))
-    logger_chirho.info("Broadcast TESTING BEGUN ALELUYA")
-    logger_chirho.info(Fore.GREEN + "Make sure stream has started and wait about 10 seconds - Hallelujah" + Fore.RESET)
-    input("Then Press Enter to continue... ALELUYA")
-
-    broadcast_transition_response_chirho = youtube_chirho.liveBroadcasts().transition(
-        part="id,status",
-        broadcastStatus="live",
-        id=insert_broadcast_response_chirho["id"]).execute()
-
-    logger_chirho.debug(
-        json.dumps(broadcast_transition_response_chirho, indent=4, sort_keys=True))
-    logger_chirho.info(Fore.BLUE + "Finished - Hallelujah")
-
-    # request_chirho = youtube_chirho.liveStreams().list(
-    #     part="snippet,contentDetails,status",
-    #     mine=True
-    # )
-    # response_chirho = request_chirho.execute()
-    #
-    # print(json.dumps(response_chirho))
+    YouTubeStreamerChirho().execute_chirho()
 
 
 if __name__ == "__main__":
